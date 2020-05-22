96dc4cbd0feae95cd224d1b2406cb27fb94b3185
Scott Kutler <61331933+skut21x-ga@users.noreply.github.com>
2020-05-22 13:18:47 -0400

    added fade

--- src/Intro.js
+++ src/Intro.js
@@ -4,14 +4,14 @@ import abstract from "./assets/abstract-small-wide.png";
 class intro extends React.Component {
   render() {
     return (
-      <div className="intro-background-color">
+      <div className="intro-background-color fade-in-fast">
         <div className="intro-overlay"></div>
         <div
-          className="intro-background"
+          className="intro-background fade-in-slow"
           style={{ backgroundImage: `url(${abstract})` }}
         >
           <a className="intro-name">Scott Kutler</a>
-          <a className="intro-subheading">Full Stack Engineer</a>
+          <a className="intro-subheading fade-in-slower">Full Stack Engineer</a>
         </div>
       </div>
     );
