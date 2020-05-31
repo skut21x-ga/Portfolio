18e1713f2df99ee979a71500cc033b1fee559df6
Scott Kutler <61331933+skut21x-ga@users.noreply.github.com>
2020-05-31 03:11:11 -0400

    updated resume section

--- src/About.js
+++ src/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import cloud from "./assets/wordcloud-small.png";
 
 class About extends React.Component {
   render() {
@@ -10,6 +11,94 @@ class About extends React.Component {
           personality and ability to problem-solve under pressure, I can serve
           as a great addition to any team.
         </h4>
+        <div class="about-flex-grid">
+          <div class="col1">
+            <h4 className="about-h4-left">Front-End</h4>
+            <h5 className="about-h5-left">
+              HTML <br />
+              CSS <br />
+              Javascript <br />
+              React <br />
+              Responsive Design <br />
+              Storybook
+            </h5>
+            <h4 className="about-h4-left">Deployment</h4>
+            <h5 className="about-h5-left">
+              Git / Github <br />
+              Heroku <br />
+              Bitbucket <br />
+              Netlify <br />
+              Amazon AWS <br />
+              Firebase <br />
+            </h5>
+            <h4 className="about-h4-left">Design</h4>
+            <h5 className="about-h5-left">
+              Final Cut Pro (Certified)
+              <br />
+              Adobe Photoshop <br />
+              Adobe InDesign
+              <br />
+              Adobe XD
+              <br />
+              Adobe Premiere
+              <br />
+              Adobe Muse
+              <br />
+            </h5>
+          </div>
+          <div class="col2 about-wordcloud">
+            <img className="about-wordcloud" src={cloud} />
+          </div>
+          <div class="col3">
+            <h4 className="about-h4-right">Backend</h4>
+            <h5 className="about-h5-right">
+              Express.js
+              <br />
+              Node.js
+              <br />
+              MongoDB
+              <br />
+              Python
+              <br />
+              RESTful API
+              <br />
+              Django
+              <br />
+              SQL
+              <br />
+              PostgreSQL
+              <br />
+              Peewee
+              <br />
+            </h5>
+            <h4 className="about-h4-right">Project Management</h4>
+            <h5 className="about-h5-right">
+              Rapid Prototyping
+              <br />
+              Google Analytics <br />
+              Google Play Console
+              <br />
+              App Store Connect
+              <br /> Jira
+              <br />
+              Trello <br />
+            </h5>{" "}
+            <h4 className="about-h4-right">Misc. Skills</h4>
+            <h5 className="about-h5-right">
+              Unix
+              <br />
+              VS Code <br />
+              MS Office (Certified)
+              <br />
+              Wordpress
+              <br />
+              Webflow CMS
+              <br />
+              Socia Media / Branding
+              <br />
+            </h5>{" "}
+          </div>
+        </div>
         <div className="resume-embed">
           <embed
             className="resume-container"
